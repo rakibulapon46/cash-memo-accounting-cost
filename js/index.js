549354bc20464784adba500cdbae37dcfa23474d
rakibulapon46 <134295199+rakibulapon46@users.noreply.github.com>
2024-03-15 04:28:27 +0600

    update all files

---
+++ js/index.js
@@ -0,0 +1,50 @@
+// finding elements
+document.addEventListener("DOMContentLoaded", function () {
+  const billingForm = document.getElementById("billing_form");
+
+  const addBtn = document.getElementById("addBtn");
+  const message = document.getElementById("message");
+  const lists = document.getElementById("lists");
+  const totalDiv = document.getElementById("totalDiv");
+  const printMemo = document.getElementById("printMemo");
+  const clearAllBtn = document.getElementById("clearAllBtn");
+  const deleteBtn = document.getElementById("deleteBtn");
+  let total = 0;
+
+  billingForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const itemName = document.getElementById("itemName").value;
+    const itemQuantity = parseInt(
+      document.getElementById("itemQuantity").value
+    );
+    const itemPrice = parseFloat(
+      document.getElementById("itemPrice").value
+    ).toFixed(2);
+
+    const itemTotal = itemQuantity * itemPrice;
+    total += itemTotal;
+
+    const itemElement = document.createElement("li");
+    itemElement.innerHTML = ` ${itemName} - ${itemQuantity} x ${itemPrice} = ${itemTotal.toFixed(
+      2
+    )} <span> <button id="deleteBtn"><i class="fa-solid fa-xmark"></i> </button></span>`;
+    lists.appendChild(itemElement);
+
+    totalDiv.textContent = `Total: ${total.toFixed(2)} Taka`;
+    billingForm.reset();
+
+    // deleteBtn.addEventListener("click", function () {
+    //   itemElement.style.display = "none";
+    //   total += itemTotal;
+    // });
+
+    clearAllBtn.addEventListener("click", function () {
+      itemElement.style.display = "none";
+      totalDiv.textContent = `Total: 0 Taka`;
+      total = 0;
+    });
+  });
+  printMemo.addEventListener("click", function () {
+    window.print();
+  });
+});
